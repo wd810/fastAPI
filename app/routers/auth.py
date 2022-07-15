3aba22aead4336535d2da49c8fc70e196c30f343
wangdian <36665764+wangdianyh@users.noreply.github.com>
2022-07-15 00:15:07 -0300

    fix schema bug

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -7,7 +7,7 @@ router = APIRouter(
     tags=['Authentication']
 )
 
-@router.post('/login')
+@router.post('/login', response_model=schema.Token)
 def login(user_credential: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     # OAuth2PasswordRequestForm contains: username and password ...
     user = db.query(models.User).filter(models.User.email == user_credential.username).first()
@@ -25,4 +25,4 @@ def login(user_credential: OAuth2PasswordRequestForm = Depends(), db: Session = 
     access_token = oauth2.create_access_token(data= {"user_id": user.id})
     
     # return the token
-    return {"token": access_token, "token_type": "bearer"}
+    return {"access_token": access_token, "token_type": "bearer"}
