a9db831e7eee71de9a3d364261bfcd79315fa17a
wangdian <36665764+wangdianyh@users.noreply.github.com>
2022-07-16 22:16:53 -0300

    clean main.py

--- app/database.py
+++ app/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 # specify database connection string
 # string format: postgresql://<username>:<password>@<ip_address/hostname>/<database_name>
@@ -20,3 +23,18 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# postgre db connection
+while True:
+    # keep connection until success
+    try:
+        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres',
+                password='123456', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("db connection was successful")
+        break
+    except Exception as error:
+        print("fail at connection to postgre")
+        print("Error: ", error)
+        # if fails, retry after 2 seconds
+        time.sleep(2)
--- app/main.py
+++ app/main.py
@@ -1,7 +1,4 @@
 from fastapi import FastAPI
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
 from . import models
 from .database import engine
 from .routers import post, user, auth
@@ -10,22 +7,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# postgre db connection
-while True:
-    # keep connection until success
-    try:
-        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres',
-                password='123456', cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("db connection was successful")
-        break
-    except Exception as error:
-        print("fail at connection to postgre")
-        print("Error: ", error)
-        # if fails, retry after 2 seconds
-        time.sleep(2)
-
-
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
@@ -33,7 +14,3 @@ app.include_router(auth.router)
 @app.get("/")
 async def root():
     return {"message": "welcome to my API"}
-
-
-
-
