4f22c4ab2517a4a8e7cb207e8b4c445dcfb5cf5d
wangdian <36665764+wangdianyh@users.noreply.github.com>
2022-07-16 21:41:32 -0300

    user get posts published by himself

--- app/routers/post.py
+++ app/routers/post.py
@@ -14,10 +14,11 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schema.Post])
-def get_posts(db: Session = Depends(get_db)):
+def get_posts(db: Session = Depends(get_db),
+              current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
-    posts = db.query(models.Post).all()
+    posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
     return posts
 
 
