1950ada2becc2206fdb9c7ff5c75145d07580ce6
wangdian <36665764+wangdianyh@users.noreply.github.com>
2022-07-18 20:11:46 -0300

    fix exist voted post

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -9,24 +9,21 @@ router = APIRouter(
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def vote(vote: schema.Vote, db: Session = Depends(database.get_db),
-        current_user: int = Depends(oauth2.get_current_user)):
+         current_user: int = Depends(oauth2.get_current_user)):
 
         vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, 
                                                   models.Vote.user_id == current_user.id)
         found_vote = vote_query.first()
 
         if vote.dir == 1:
-            try:
-                if found_vote:
-                    raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                                        detail=f"user: {current_user.id} has already liked post {vote.post_id}")
-                new_vote = models.Vote(post_id = vote.post_id, user_id=current_user.id)
-                db.add(new_vote)
-                db.commit()
-                return {"message": "successfully vote"}
-            except:
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                    detail=f"post {vote.post_id} does not exist")
+            
+            if found_vote:
+                raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                    detail=f"user: {current_user.id} has already liked post {vote.post_id}")
+            new_vote = models.Vote(post_id = vote.post_id, user_id=current_user.id)
+            db.add(new_vote)
+            db.commit()
+            return {"message": "successfully vote"}
 
         else:
             if not found_vote:
