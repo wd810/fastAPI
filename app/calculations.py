3a2facf1b318e2dfbf5a44da0e64a6bcc7067691
wangdian <36665764+wangdianyh@users.noreply.github.com>
2022-07-27 23:11:57 -0300

    import pytest.fixture

--- app/calculations.py
+++ app/calculations.py
@@ -9,3 +9,18 @@ def multiply(num1: int, num2: int):
 
 def devide(num1: int, num2: int):
     return num1 / num2
+
+
+
+class BankAccount():
+    def __init__(self, starting_balance=0):
+        self.balance = starting_balance
+    
+    def deposit(self, amount):
+        self.balance += amount
+    
+    def withdraw(self, amount):
+        self.balance -= amount
+
+    def collect_interest(self):
+        self.balance *= 1.1
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,8 +1,23 @@
-from app.calculations import add, substract, multiply, devide
+import pytest
+from app.calculations import add, substract, multiply, devide, BankAccount
 
-def test_add():
+@pytest.fixture
+def zero_bank_account():
+    print("creating an empty account")
+    return BankAccount()
+
+@pytest.fixture
+def bank_account():
+    return BankAccount(50)
+
+@pytest.mark.parametrize("num1, num2, expected", [
+    (3, 34, 37), 
+    (7, 1, 8),
+    (12, 4, 16)
+])
+def test_add(num1, num2, expected):
     # print("testing adding function")
-    assert add(5, 3) == 8
+    assert add(num1, num2) == expected
 
 def test_substract():
     assert substract(9, 4) == 5
@@ -14,3 +29,25 @@ def test_multiply():
 
 def test_devide():
     assert devide(10, 2) == 5
+
+
+def test_bank_set_initial_amount(bank_account):
+    assert bank_account.balance == 50
+
+def test_bank_default_amount(zero_bank_account):
+    print("testing my bank account")
+    assert zero_bank_account.balance == 0
+
+def test_withdraw(bank_account):
+    bank_account.withdraw(20)
+    assert bank_account.balance == 30
+
+
+def test_deposit(bank_account):
+    bank_account.deposit(30)
+    assert bank_account.balance == 80
+
+
+def test_collect_interest(bank_account):
+    bank_account.collect_interest()
+    assert round(bank_account.balance, 6) == 55
