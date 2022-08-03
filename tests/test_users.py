78b8fc522def3e4feb5364cf29ab531e4d9d8e7a
wangdian <36665764+wangdianyh@users.noreply.github.com>
2022-08-02 23:12:53 -0300

    test with testclient

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,2 +1,20 @@
 from fastapi.testclient import TestClient
+from app.main import app
+from app import schema
 
+client = TestClient(app)
+
+def test_root():
+    res = client.get("/")
+    print(res.json)
+    assert res.json().get('message') == 'welcome to my API'
+    assert res.status_code == 200
+
+
+
+def test_creat_user():
+    res = client.post("/users/", json={"email": "hello@456.com", "password": "pass1123"})
+    
+    test_user = schema.UserOut(**res.json())
+    assert test_user.email == "hello@456.com"
+    assert res.status_code == 201
