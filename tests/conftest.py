df5f34718d4f39d29b9f9bb9a9b02fa8d734b460
wangdian <36665764+wangdianyh@users.noreply.github.com>
2022-08-05 00:44:39 -0300

    test posts api

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,12 @@
+from turtle import title
 from fastapi.testclient import TestClient
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.main import app
 from app.database import get_db, Base
+from app.oauth2 import create_access_token
+from app import models
 
 
 
@@ -27,7 +30,6 @@ def session():
         db.close()
 
 
-
 @pytest.fixture()
 def client(session):
     def overrid_get_db():
@@ -37,3 +39,69 @@ def client(session):
             session.close()
     app.dependency_overrides[get_db] = overrid_get_db
     yield TestClient(app)
+
+
+'''
+make every test function independently
+'''
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "test_login@123.com", "password": "password123"}
+    res = client.post("/users/", json=user_data)
+    
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+
+@pytest.fixture
+def token(test_user):
+    return create_access_token({'user_id': test_user['id']})
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {
+        **client.headers,
+        "Authorization": f"Bearer {token}"
+    }
+
+    return client
+
+
+@pytest.fixture
+def get_test_posts(test_user, session):
+    posts_data = [
+        {
+            "title": "1st post",
+            "content": "I love CA",
+            "owner_id": test_user['id']
+        },
+        {
+            "title": "2nd post",
+            "content": "I love Busan",
+            "owner_id": test_user['id']
+        },
+        {
+            "title": "3rd post",
+            "content": "I love So",
+            "owner_id": test_user['id']
+        },
+    ]
+
+
+    def create_post_model(post):
+        return models.Post(**post)
+
+    posts_map = map(create_post_model, posts_data)
+    # convert map to list
+    posts = list(posts_map)
+
+    # add all posts to db
+    session.add_all(posts)
+    # session.add_all([models.Post(title="1 post", content="i love ca", owner_id=test_user['id']),
+    #                 models.Post(title="2 post", content="i love busan", owner_id=test_user['id']),
+    #                 models.Post(title="3 post", content="i love sl", owner_id=test_user['id'])])
+
+    session.commit()
+    return session.query(models.Post).all()
---
+++ tests/test_posts.py
@@ -0,0 +1,65 @@
+import json
+import pytest
+from app import schema
+
+def test_get_all_posts(authorized_client, get_test_posts):
+    res = authorized_client.get('/posts/')
+
+    def validate(post):
+        return schema.PostOut(**post)
+    
+    posts_map = map(validate, res.json())
+    posts_list = list(posts_map)
+
+    assert len(res.json()) == len(get_test_posts)
+    assert res.status_code == 200
+
+
+def test_unauthorized_user_get_all_posts(client, get_test_posts):
+    res = client.get('/posts')
+    assert res.status_code == 401
+
+def test_unauthorized_user_get_one_posts(client, get_test_posts):
+    res = client.get(f'/posts/{get_test_posts[0].id}')
+    assert res.status_code == 401
+
+def test_get_post_not_exist(authorized_client):
+    res = authorized_client.get("/posts/9999")
+    assert res.status_code == 404
+
+def test_get_one_post(authorized_client, get_test_posts):
+    res = authorized_client.get(f'/posts/{get_test_posts[0].id}')
+    print(res.json())
+
+    post = schema.PostOut(**res.json())
+    assert post.Post.id == get_test_posts[0].id
+
+
+@pytest.mark.parametrize("title, content, published", [
+    ("i love ca", "i love busan", True),
+    ("i love ca", "i love busan", False),
+    ("i love ca", "i love busan", True),
+])
+def test_create_post(authorized_client, test_user, title, content, published):
+    res = authorized_client.post('/posts/', json={"title": title, "content": content, "published": published})
+
+    created_post = schema.Post(**res.json())
+
+    assert res.status_code == 201
+    assert created_post.title == title
+    assert created_post.content == content
+    assert created_post.published == published
+    assert created_post.owner_id == test_user['id']
+
+
+def test_create_post_default_published_true(authorized_client, test_user):
+    res = authorized_client.post('/posts/',
+                                json={"title": "new title", "content": "new content"})
+
+    created_post = schema.Post(**res.json())
+
+    assert res.status_code == 201
+    assert created_post.title == "new title"
+    assert created_post.content == "new content"
+    assert created_post.published == True
+    assert created_post.owner_id == test_user['id']
